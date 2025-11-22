24475036fff72163482e91e8fc728450f144becf
kovacsnandor <kovacsnandor@gmail.com>
2025-11-22 21:52:14 +0100

    Playingsport api

---
+++ server/database/seeders/PlayingsportSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PlayingsportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
