75ad4a29caa45701cc9a3cc211f0d6e1a7aa96ec
kovacsnandor <kovacsnandor@gmail.com>
2025-11-22 22:58:01 +0100

    Student migrate

--- server/app/Models/Student.php
+++ server/app/Models/Student.php
@@ -9,4 +9,30 @@ class Student extends Model
 {
     /** @use HasFactory<\Database\Factories\StudentFactory> */
     use HasFactory;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'diakNev',
+        'schoolclassId',
+        'neme',
+        'iranyitoszam',
+        'lakHelyseg',
+        'lakCim',
+        'szulHelyseg',
+        'szulDatum',
+        'igazolvanyszam',
+        'atlag',
+        'osztondij',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'szulDatum' => 'date',
+            'neme' => 'boolean',
+            'iranyitoszam' => 'string',
+            'atlag' => 'decimal:1',
+            'osztondij' => 'decimal:0',
+        ];
+    }
 }
--- server/database/migrations/2025_11_22_205101_create_students_table.php
+++ server/database/migrations/2025_11_22_205101_create_students_table.php
@@ -12,7 +12,23 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id();
+            $table->integer('id')->autoIncrement();
+            $table->primary('id');
+            $table->string('diakNev', 75)->notNull();
+            $table->integer('schoolclassId')->nullable()->default(null);
+            $table->foreign('schoolclassId') // Az idegen kulcs oszlopa
+                  ->references('id')         // A hivatkozott oszlop a szülő táblában
+                  ->on('schoolclasses')      // A szülő tábla neve
+                  ->onDelete('restrict');    // Ha hivatkoznak rá, a törlés blokkolva van.
+            $table->boolean('neme')->default(false);
+            $table->string('iranyitoszam', 5)->nullable()->default(null);
+            $table->string('lakHelyseg', 75)->nullable()->default(null);
+            $table->string('lakCim', 100)->nullable()->default(null);
+            $table->string('szulHelyseg', 75)->nullable()->default(null);
+            $table->date('szulDatum')->nullable()->default(null);
+            $table->string('igazolvanyszam', 25)->nullable()->default(null);
+            $table->decimal('atlag', 2, 1)->default(0.0);
+            $table->decimal('osztondij', 10, 0)->default(0);
             $table->timestamps();
         });
     }
