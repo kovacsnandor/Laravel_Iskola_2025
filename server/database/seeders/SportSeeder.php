5fa6e27a7c5f3337b03ef8943cb33860ffcba72e
kovacsnandor <kovacsnandor@gmail.com>
2025-11-23 11:48:31 +0100

    Helper: CsvReader

--- server/database/seeders/SportSeeder.php
+++ server/database/seeders/SportSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Helpers\CsvReader;
+use App\Models\Sport;
 
 class SportSeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class SportSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $fileName = 'csv\products.csv';
+        $delimiter = ';';
+
+        $data = CsvReader::csvToArray($fileName, $delimiter);
+
+        if (Sport::count() === 0) {
+            Sport::factory()->createMany($data);
+        }
     }
 }
