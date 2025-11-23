b015fba1a917cae4feea01ef881e4cea57413fdb
kovacsnandor <kovacsnandor@gmail.com>
2025-11-23 12:35:43 +0100

    DatabasSeeder javítás

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -16,16 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         //Mielőtt seedelünk, minden táblát töröljünk le.
         DB::statement('DELETE FROM users');
         DB::statement('DELETE FROM playingsports');
         DB::statement('DELETE FROM students');
         DB::statement('DELETE FROM schoolclasses');
         DB::statement('DELETE FROM sports');
-
-
 
         //Ami Seeder osztály itt fel van sorolva, annak lefut a run() metódusa
         $this->call([
