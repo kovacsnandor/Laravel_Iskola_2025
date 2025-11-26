f966465f3c7c3c344ad2f59ed9228b697124235b
kovacsnandor <kovacsnandor@gmail.com>
2025-11-26 23:49:06 +0100

    test_students_schoolclasses_relationships()

--- server/tests/Unit/DatabaseTest.php
+++ server/tests/Unit/DatabaseTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Models\Schoolclass;
+use App\Models\Student;
 use Illuminate\Support\Facades\DB;
 // use PHPUnit\Framework\TestCase;
 use Tests\TestCase;
@@ -14,7 +16,7 @@ class DatabaseTest extends TestCase
     //Bármit változtatunk az adatbázison a teszt során, az vissza lesz vonva
     use DatabaseTransactions;
 
-    
+
     public function test_database_creation_end_tables_exists()
     {
         $databaseNameConn = DB::connection()->getDatabaseName();
@@ -29,7 +31,7 @@ class DatabaseTest extends TestCase
         $this->assertDatabaseHas('playingsports');
         $this->assertDatabaseHas('sports');
         $this->assertDatabaseHas('users');
-        echo PHP_EOL."\tAdatbázis -> DB_DATABASE={$databaseNameEnv} | adatbázis: {$databaseNameConn}";
+        echo PHP_EOL . "\tAdatbázis -> DB_DATABASE={$databaseNameEnv} | adatbázis: {$databaseNameConn}";
     }
 
     public function test_sports_table_structure()
@@ -40,7 +42,7 @@ class DatabaseTest extends TestCase
         // Ellenőrizzük a mezőket és azok típusait
         $this->assertTrue(Schema::hasColumn('sports', 'id'));
         $this->assertTrue(Schema::hasColumn('sports', 'sportNev'));
-        $this->assertEquals('bigint', Schema::getColumnType('sports', 'id'));
+        $this->assertEquals('bigint', Schema::getColumnType('sports', 'id'), "sports: Nem bigint az id típusa");
         //dd(Schema::getColumnType('sports', 'sportNev'));
         $this->assertEquals('varchar', Schema::getColumnType('sports', 'sportNev'));
 
@@ -49,8 +51,78 @@ class DatabaseTest extends TestCase
         //Elsődleges kulcs
         $indexes = DB::select('SHOW INDEX FROM sports');
         $primaryIndex = collect($indexes)->firstWhere('Key_name', 'PRIMARY');
-        $this->assertNotNull($primaryIndex);
+        $this->assertNotNull($primaryIndex, "Nincs elsődleges kulcs");
 
+        //Egyedi index: sportNev
+        $uniqueIndex = collect($indexes)->firstWhere('Key_name', 'sports_sportnev_unique');
+
+        $this->assertNotNull($uniqueIndex, "Hiányzik az egyedi index a 'sports.sportNev' oszlopon.");
     }
 
+
+    public function test_students_schoolclasses_relationships()
+    {
+
+        //A diák tábla kapcsolatai
+        $tableName = "students";
+        $foreignKeyName = "schoolclassId";
+        $databaseName = env('DB_DATABASE');
+        $contstraint_name = "PRIMARY";
+
+        $query = "
+            SELECT 
+                TABLE_NAME,
+                COLUMN_NAME,
+                CONSTRAINT_NAME,
+                REFERENCED_TABLE_NAME,
+                REFERENCED_COLUMN_NAME
+            FROM 
+                information_schema.KEY_COLUMN_USAGE
+            WHERE
+                TABLE_NAME = ? and COLUMN_NAME = ? and CONSTRAINT_SCHEMA = ? and CONSTRAINT_NAME <> ?";
+
+        $rows = DB::select($query, [$tableName, $foreignKeyName, $databaseName, $contstraint_name]);
+        // dd($rows);
+        //Idegen kulcs neve: osztalyId
+        $this->assertEquals('schoolclassId', $rows[0]->COLUMN_NAME);
+        //Referencia tábla neve: osztalies
+        $this->assertEquals('schoolclasses', $rows[0]->REFERENCED_TABLE_NAME);
+        //Referencia oszlop neve: id
+        $this->assertEquals('id', $rows[0]->REFERENCED_COLUMN_NAME);
+
+
+        //Készítünk egy osztályt
+        $dataSchoolclass =
+            [
+                'osztalyNev' => '99.d'
+            ];
+        $schoolclass = Schoolclass::factory()->create($dataSchoolclass);
+
+        //Az új osztállyal készítek egy diákot
+        $dataStudent =
+            [
+                'diakNev' => 'Rudi',
+                'schoolclassId' => $schoolclass->id,
+                'neme' => true,
+                'iranyitoszam' => '1234',
+                'lakHelyseg' => 'Szolnok',
+                'lakCim' => 'Virág utca 5.',
+                'szulHelyseg' => 'Szolnok',
+                'szulDatum' => '2018-01-12',
+                'igazolvanyszam' => 'ab1234567',
+                'atlag' => 3.5,
+                'osztondij' => 5000
+            ];
+        $student = Student::factory()->create($dataStudent);
+
+        //visszakeressük a diákot
+        $student = DB::table('students')
+            ->where('id',  $student->id)
+            ->first();
+
+        //A megtalált diák osztalyId-je megegyezik a új osztály id-jével        
+        $this->assertEquals($schoolclass->id, $student->schoolclassId);
+        // dd($diak);
+
+    }
 }
