bd0658d7bb99870c60a799c1d799332621e4e357
kovacsnandor <kovacsnandor@gmail.com>
2025-12-08 18:15:45 +0100

    Státus ellenőrzés javítás

--- server/tests/Feature/ExampleTest.php
+++ server/tests/Feature/ExampleTest.php
@@ -4,8 +4,9 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Tests\TestBase;
 
-class ExampleTest extends TestCase
+class ExampleTest extends TestBase
 {
     /**
      * A basic test example.
@@ -24,4 +25,32 @@ class ExampleTest extends TestCase
         $response->assertStatus(200);
         $response->assertSee("API");
     }
+
+    public function test_user_login_logout()
+    {
+        // A login metódust már tudod hívni a $this-en keresztül:
+        $response = $this->login();
+
+        $response->assertStatus(200);
+        $response->assertSuccessful();
+
+        // $token = $response->json('data')['token'];
+        $token = $this->myGetToken($response);
+
+        //Lekérjük a user tábla adatait
+        $uri = '/api/users';
+        $response = $this->myGet($uri, $token);
+        $response->assertStatus(200);
+        $response->assertSuccessful();
+
+
+        $response = $this->logout($token);
+        $response->assertStatus(200);
+        $response->assertSuccessful();
+
+
+        // ... folytasd a tesztet
+    }
+
+
 }
---
+++ server/tests/TestBase.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Tests;
+
+abstract class TestBase extends TestCase
+{
+
+    protected function login(string $email = "admin@example.com", string $password = "123")
+    {
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ];
+        $uri = '/api/users/login';
+        $data = [
+            'email' => $email,
+            'password' => $password
+        ];
+
+        //Bejelentkezés
+        $response = $this
+            ->withHeaders($headers)
+            ->postJson($uri, $data);
+
+        return $response;
+    }
+
+
+    protected function logout($token)
+    {
+        $headers = [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+        $uri = '/api/users/logout';
+
+        $response = $this
+            ->withHeaders($headers)
+            ->postJson($uri);
+        return $response;
+    }
+
+    protected function myGetToken($response)
+    {
+        $token = $response->json('data')['token'];
+        return $token;
+    }
+
+    protected function myGet(string $uri, string $token)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+        //Egy nem védett útvonalra küldünk egy kérést
+        $uri = '/api/sports';
+        $response = $this
+            ->withHeaders($headers)
+            ->get($uri);
+        return $response;    
+    }
+
+    protected function myPost(string $uri, array $data, string $token)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+        
+        $response = $this
+            ->withHeaders($headers)
+            ->postJson($uri, $data); // postJson() az adatok JSON-ként való küldéséhez
+            
+        return $response;
+    }
+
+    protected function myPatch(string $uri, array $data, string $token)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+        
+        $response = $this
+            ->withHeaders($headers)
+            ->patchJson($uri, $data); // patchJson() az adatok JSON-ként való küldéséhez
+            
+        return $response;
+    }
+
+    protected function myDelete(string $uri, string $token)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+        
+        $response = $this
+            ->withHeaders($headers)
+            ->delete($uri); // delete() metódus használata
+            
+        return $response;
+    }
+
+    // Ide jöhetnek más közös segédfüggvények is (pl. CRUD metódusok)
+}
